e6aedd04fb0c3718f11ab35c5eb65a561c4ad644
StewartBarker <Stew.Barker@outlook.com>
2022-12-06 08:58:27 +0000

    tidying up

--- AoCDay6/Program.cs
+++ AoCDay6/Program.cs
@@ -8,11 +8,10 @@ var packetLength = 14;
 
 foreach(var line in lines)
 {
-    for(var x=0; x<line.Length-packetLength;x++)
+    for(var x = 0; x < line.Length - packetLength; x++)
     {
         if (valsAreDistinct(line.Skip(x).Take(packetLength).ToList()))
         {
-            Console.WriteLine($"First marker x={x}: marker= line=[{line}]");
             total += (x + packetLength);
             break;
         }     
